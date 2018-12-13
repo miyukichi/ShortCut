6496316a91f98170ddad9c76301d2821f65fb5d8
ntt <ntt@BI-23555.local>
2018-12-13 15:11:23 +0900

    メイラー起動のボタンを２つ追加

--- ShortCut/ViewController.swift
+++ ShortCut/ViewController.swift
@@ -7,32 +7,63 @@
 //
 
 import UIKit
+import MessageUI
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
-    @IBAction func call() {
-        UIApplication.shared.open(URL(string: "telprompt://00000000000")!, completionHandler: nil)
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
-    @IBAction func call2() {
+    
+    @IBAction func sendMailOne(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["xxx@xxx"]) // 宛先アドレス
+            mail.setSubject("お問い合わせ") // 件名
+            mail.setMessageBody("ここに本文が入ります。", isHTML: true) // 本文
+            
+            present(mail, animated: true, completion: nil)
+        } else {
+            print("送信できません")
+        }
+}
+    
+    @IBAction func sendMailTwo(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["xxx@xxx.xxx"]) // 宛先アドレス
+            mail.setSubject("お問い合わせ") // 件名
+            mail.setMessageBody("ここに本文が入ります。", isHTML: true) // 本文
+            
+            present(mail, animated: true, completion: nil)
+        } else {
+            print("送信できません")
+        }
+    }
+    
+func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+    
+
+    @IBAction func callOne() {
+        UIApplication.shared.open(URL(string: "telprompt://09011231955")!, completionHandler: nil)
+    }
+    
+    @IBAction func callTwo() {
         UIApplication.shared.open(URL(string: "telprompt://00000000000")!, completionHandler: nil)
     }
 }
-
-
-
-
